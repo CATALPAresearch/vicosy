24821b79e9d8f8ee83af718dd3b3234a927ad6af
Jost Fromhage <jostfromhage@web.de>
2020-10-13 20:52:43 +0200

    working on form trainerscript

--- client/src/components/video-session/TrainerSessionCreation/TrainerSessionCreator.js
+++ client/src/components/video-session/TrainerSessionCreation/TrainerSessionCreator.js
@@ -1,6 +1,7 @@
 import { connect } from "react-redux";
 import React, { Component } from "react";
 import { createSession, ownSocketId } from "../../../socket-handlers/api";
+import "./TrainerSessionCreator.css";
 import { LOG_ERROR } from "../../logic-controls/logEvents";
 import {
   SESSION_DEFAULT,
@@ -73,63 +74,114 @@ class SessionCreatorTrainer extends Component {
 
     return (
       <form onSubmit={this.onSubmit.bind(this)} className="mb-2">
+        <h1>Scripteinstellungen</h1>
         <div class="row">
           <div class="col-sm-9 border bg-light">
-          <span className="hFlexLayout">
-              <label htmlFor="sessionname">
+            <div class="row">
+              <div class="col-6 col-sm-3"><h3 htmlFor="sessionname">
                 Session name
-          </label>
+          </h3></div>
+              <div class="col-6 col-sm-8">
+                <input
+                  id="sessionname"
+                  value={this.state.sessionname}
+                  type="text"
+                  className="form-control form-control-lg mr-sm-2"
+                  placeholder="Session Name"
+                  onChange={this.handleChange.bind(this)}
+                />
+              </div>
+              <div class="w-100"></div>
+              <div class="col-6 col-sm-3">
+                <h3 htmlFor="videourl">
+                  Url Input
+                </h3>
+              </div>
+              <div class="col-6 col-sm-8">
+                <input
+                  ref={this.urlInput}
+                  id="videourl"
+                  value={this.state.videourl}
+                  type="text"
+                  className="form-control form-control-lg mr-sm-2"
+                  placeholder="Video URL"
+                  onChange={this.handleChange.bind(this)}
+                />
+                <input
+                  type="button"
+                  className="btn btn-outline-secondary mr-sm-2"
+                  value="Clear"
+                  onClick={this.onClearUrl.bind(this)}
+                />
+              </div>
 
-              <input
-                id="sessionname"
-                value={this.state.sessionname}
-                type="text"
-                className="form-control form-control-lg mr-sm-2"
-                placeholder="Session Name"
-                onChange={this.handleChange.bind(this)}
-              />
-            </span>
+              <div class="w-100"></div>
 
-            <label className="sr-only" htmlFor="videourl">
-              Url Input
-          </label>
-
-            <input
-              ref={this.urlInput}
-              id="videourl"
-              value={this.state.videourl}
-              type="text"
-              className="form-control form-control-lg mr-sm-2"
-              placeholder="Video URL"
-              onChange={this.handleChange.bind(this)}
-            />
-
-            <input
-              type="button"
-              className="btn btn-outline-secondary mr-sm-2"
-              value="Clear"
-              onClick={this.onClearUrl.bind(this)}
-            />
-
-            <select ref={this.sessionTypeRef} className="form-control mr-sm-2">
-              <option
-                value={SESSION_PEER_TEACHING}
-                title={
-                  scriptsEnabled
-                    ? "Controlled learning via collaboration script 'Peer teaching'. Description... Required peers: 2"
-                    : "Not available for guest accounts"
-                }
-                disabled={!scriptsEnabled}
-              >
-                Scripted Peer Teaching
+              <div class="col-6 col-sm-3"><h3 htmlFor="sessionname">
+                Sessiontyp
+            </h3>
+              </div>
+              <div class="col-6 col-sm-8">
+                <select ref={this.sessionTypeRef} className="form-control mr-sm-2">
+                  <option
+                    value={SESSION_PEER_TEACHING}
+                    title={
+                      scriptsEnabled
+                        ? "Controlled learning via collaboration script 'Peer teaching'. Description... Required peers: 2"
+                        : "Not available for guest accounts"
+                    }
+                    disabled={!scriptsEnabled}
+                  >
+                    Scripted Peer Teaching
             </option>
-              <option
-                value={SESSION_DEFAULT}
-                title="Uncontrolled video conversation without member limit."
-              >
-                Default Video Conversation
+                  <option
+                    value={SESSION_DEFAULT}
+                    title="Uncontrolled video conversation without member limit."
+                  >
+                    Default Video Conversation
             </option>
-            </select>
+                </select>
+              </div>
+              <div class="w-100"></div>
+
+              <div class="col-6 col-sm-3">
+                <h3 htmlFor="phase0">
+                  Vorstellungsphase erwünscht
+                </h3>
+              </div>
+              <div class="col-6 col-sm-8">
+              </div>
+              <div class="w-100"></div>
+              <div class="col-6 col-sm-3">
+                <h3 htmlFor="phase5">
+                  Vertiefungsphase erwünscht
+                </h3>
+              </div>
+              <div class="col-6 col-sm-8">
+              </div>
+              <div class="w-100"></div>
+              <div class="col-6 col-sm-3">
+                <h3 htmlFor="gruppengroesse">
+                  Gruppengröße
+                </h3>
+              </div>
+              <div class="col-6 col-sm-8">
+              </div>
+              <div class="w-100"></div>
+              <div class="col-6 col-sm-3">
+                <h3 htmlFor="gruppengroesse">
+                  Gruppenmix
+                </h3>
+              </div>
+              <div class="col-6 col-sm-8">
+              </div>
+            </div>
+
+
+
+
+
+
 
             <input
               type="submit"
