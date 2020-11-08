71bb6343f7af60138269674ea295251fe34851b1
Jost Fromhage <jostfromhage@web.de>
2020-11-08 22:59:02 +0100

    Fehlermeldung bei Einschreibung ins Script und Script clear noch machen

--- client/src/components/trainerlobby/TrainerLobby.js
+++ client/src/components/trainerlobby/TrainerLobby.js
@@ -4,7 +4,7 @@ import RoomComponent from "../controls/RoomComponent";
 import TrainerScriptCreator from "../script-options/TrainerScriptCreation/TrainerScriptCreator";
 import TrainerSessionList from "../video-session/TrainerSessionList";
 import ScriptListElement from "./ScriptListElement";
-import { getScriptsByUserId } from "../../actions/scriptActions"
+import { getScriptsByUserId, deleteAllScripts, deleteScript } from "../../actions/scriptActions"
 import { Link } from "react-router-dom";
 import "./TrainerLobby.css";
 import Logger from "../logic-controls/Logger";
@@ -18,6 +18,9 @@ class TrainerLobby extends Component {
     }
     //this.setListElements.bind(this)
     this.props.getScriptsByUserId(this.props.auth.user.id);
+    //to delete all scripts
+    // this.props.deleteAllScripts(this.props.auth.user.id);
+    //  this.onClick = this.handleCLick.bind(this);
 
   }
   setListElements(newscripts) {
@@ -26,21 +29,35 @@ class TrainerLobby extends Component {
     this.setState({ scripts: newscripts });
   }
 
+  handleCLick(e) {
+    const { id, value } = e.target;
+    this.props.history.push("/newtrainerscript/?" + value);
+  }
+
+  deleteScript(e) {
+    const { id, value } = e.target;
+    this.props.deleteScript(value);
+    this.props.getScriptsByUserId(this.props.auth.user.id);
+
+  }
 
 
   render() {
+
     var participants = null;
     //this.setListElements.bind(this)
-    this.props.getScriptsByUserId(this.props.auth.user.id);
+    // this.props.getScriptsByUserId(this.props.auth.user.id);
     // console.log(this.props.script.scripts);
     if (this.props.script.scripts) {
       var scriptsArray = Object.keys(this.props.script.scripts);
       participants = this.props.script.scripts.map(script => {
-
         return <ScriptListElement
+          id={script._id}
           key={script._id}
           name={script.scriptName}
           videorul={script.videourl}
+          editScript={this.handleCLick.bind(this)}
+          deleteScript={this.deleteScript.bind(this)}
 
         />
       })
@@ -56,20 +73,19 @@ class TrainerLobby extends Component {
         <h1>Trainerlobby</h1>
         {<RoomComponent roomId="trainerlobby" component={TrainerSessionList} />}
         <Link to="/newtrainerscript" className="btn btn-lg btn-info mr-2">
-          Neue Session
+          Neues Script
         </Link>
-
+        <br></br>
         <div className="list-group">
+          {
+            participants
+          }
 
         </div>
 
 
 
-        {
-          participants
 
-
-        }
       </div>
     );
   }
@@ -84,6 +100,6 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  { getScriptsByUserId },
+  { getScriptsByUserId, deleteAllScripts, deleteScript },
   null
 )(TrainerLobby);
