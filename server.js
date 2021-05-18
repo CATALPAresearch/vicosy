33c6472b13f01e8471417e2bcb000a3bc3b9be73
jostfromhage@web.de <jostfromhage@web.de>
2021-05-18 22:38:59 +0200

    wieder port 5000

--- server.js
+++ server.js
@@ -224,7 +224,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 var server;
-const port = process.env.PORT || 5010;
+const port = process.env.PORT || 5000;
 
 //// logger api
 //Initialize server instance
