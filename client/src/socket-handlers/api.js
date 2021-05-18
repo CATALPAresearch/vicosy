ca31de5d501b1cd2e75f8b1a1d9e5aa9e0a403fb
jostfromhage@web.de <jostfromhage@web.de>
2021-05-18 21:36:29 +0200

    port

--- client/src/socket-handlers/api.js
+++ client/src/socket-handlers/api.js
@@ -19,11 +19,11 @@ const isSecure = window.location.protocol === "https:";
 
 // TODO: ssl set openSocket secure: true
 const getHost = (path, token, params = "") => {
-  var port = 5000;
+  var port = 443;
   if (window.location.hostname !== "localhost" && window.location.port)
     port = window.location.port;
 
-  return `wss//${window.location.hostname
+  return `${window.location.protocol}//${window.location.hostname
     }:${port}${path}${token ? `?token=${token}` : ""}${params}`;
 };
 
