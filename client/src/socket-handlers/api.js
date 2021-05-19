b960e0ddc721c0269aa1291c0d116c2690bf9639
jostfromhage@web.de <jostfromhage@web.de>
2021-05-19 21:19:58 +0200

    Doppelpunkt

--- client/src/socket-handlers/api.js
+++ client/src/socket-handlers/api.js
@@ -25,7 +25,7 @@ const getHost = (path, token, params = "") => {
   if (window.location.hostname !== "localhost" && window.location.port)
     port = window.location.port;
 
-  return `${window.location.protocol}//${window.location.hostname
+  return `${window.location.protocol}://${window.location.hostname
     }:${port}${path}${token ? `?token=${token}` : ""}${params}`;
 };
 
