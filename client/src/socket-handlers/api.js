efc475683a90e162562962cd01cb6365dd6ef627
jostfromhage@web.de <jostfromhage@web.de>
2021-05-19 22:02:31 +0200

    slash

--- client/src/socket-handlers/api.js
+++ client/src/socket-handlers/api.js
@@ -24,7 +24,7 @@ const getHost = (path, token, params = "") => {
     port = window.location.port;
 
   return `${window.location.protocol}//${window.location.hostname
-    }/:${port}${path}${token ? `?token=${token}` : ""}${params}`;
+    }${path}${token ? `?token=${token}` : ""}${params}`;
 };
 
 const connectToP2PSignaler = connectCb => {
