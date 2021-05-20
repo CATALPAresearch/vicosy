481dd64c4c6dbf09d20a44595c430edb02a0eb03
jostfromhage@web.de <jostfromhage@web.de>
2021-05-20 08:59:02 +0200

    fs

--- config/ssl_cert.js
+++ config/ssl_cert.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 module.exports = {
     key : fs.readFileSync("/usr/local/psa/var/modules/letsencrypt/etc/live/charming-payne.46-163-74-68.plesk.page/privkey.pem"), 
     cert: fs.readFileSync("/usr/local/psa/var/modules/letsencrypt/etc/live/charming-payne.46-163-74-68.plesk.page/cert.pem")
