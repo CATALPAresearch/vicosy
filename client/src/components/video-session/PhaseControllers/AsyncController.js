4529b1abe97f2b166deb3186a7439543bfdfefa7
jostfromhage@web.de <jostfromhage@web.de>
2021-05-30 11:34:23 +0200

    sync wechesel

--- client/src/components/video-session/PhaseControllers/AsyncController.js
+++ client/src/components/video-session/PhaseControllers/AsyncController.js
@@ -1,4 +1,4 @@
-import { connect } from "react-redux";
+  import { connect } from "react-redux";
 import React, { Component } from "react";
 import { setAsyncTime } from "../../../actions/localStateActions";
 import { TIME_UPDATE } from "../AbstractVideoEvents";
